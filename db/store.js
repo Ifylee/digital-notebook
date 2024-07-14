18f5717aa59b57feffd11e0cf71141872e28b199
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-07-14 08:45:38 -0500

    added codes to store.js

--- db/store.js
+++ db/store.js
@@ -8,14 +8,24 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
     read() {
-
+        return readFileAsync("db/db.json", "utf8")
     }
 
     write() {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
 
     }
 
     getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+            try {
+                parsedNotes.push(JSON.parse(notes))
+
+            }.catch(error) {
+                parsedNotes = []
+            }
+        })
 
     }
 
