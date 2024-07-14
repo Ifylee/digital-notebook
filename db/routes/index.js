e5805e24a785ab7d98f662d50dc8f2bab7f81ada
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-07-14 10:24:12 -0500

    added more files

--- db/routes/index.js
+++ db/routes/index.js
@@ -1,34 +1,18 @@
 const router = require("express").Router();
-const store = require("../db/store");
-
-const store = require(".db/store");
-router.get("/api/notes", (req,res) => {
-    store.getNotes().then((notes) => {
-        return res.status(200).json(notes)
-    }).catch((error) => res.status(500).json(error))
-})
+const path = require("path");
 
 
-router.post("/api/notes", (req,res) => {
-    store.addNotes(req.body).then((notes) => {
-        return res.status(200).json(notes)
-    }).catch((error) => res.status(500).json(error))
-})
+const apiRoutes = require("./apiRoutes");
 
-
-router.delete("/api/notes/:id", (req,res) => {
-    store.removeNotes(req.params,id).then(() => {
-        return res.status(200).json({delete: true, id: req.params.id})
-    }).catch((error) => res.status(500).json(error))
-})
+router.use("/api", apiRoutes);
 
 // HTML Route
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
 
 router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })    
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path");
 
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +10,8 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://licalhost:${PORT}`)
